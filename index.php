44fdda4a6afbd916c6b33e25d6fc214f0956e2c3
crisvitoria <crisa.vitoria@hotmail.com>
2023-11-22 16:33:11 -0300

    alteracoes

--- index.php
+++ index.php
@@ -15,19 +15,19 @@
     <div class="mx-10">
       <div class="lg:flex justify-center">
         <div class="text-center">
-          <div class="text-center">
-            <h1 class="text-4xl font-semibold leading-[50px] tracking-wide text-transparent bg-clip-text bg-gradient-to-l from-pink-400 to-blue-600 mb-10">
-              Página Inicial - Livraria
-            </h1>
-          </div>
+          <h1 class="text-4xl font-semibold leading-[50px] tracking-wide text-transparent bg-clip-text bg-gradient-to-l from-pink-400 to-blue-600 mb-10">
+            Página Inicial - Livraria
+          </h1>
         </div>
       </div>
     </div>
   </section>
   <!-- Section End -->
 
-<!--Cadastro de Autores-->
+
 <div class="border w-full h-40 flex items-center justify-center">
+  <div class="margendireita">
+    <!--Cadastro de Autores-->
     <a href="cadautores.php" class="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50">
     <span class="absolute left-0 block w-full h-0 transition-all bg-indigo-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
     <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
@@ -37,23 +37,9 @@
     </span>
     <span class="relative">Cadastro de Autores</span>
     </a>
-</div>
-
-<!--Cadastro de Livros-->
-<div class="border w-full h-40 flex items-center justify-center">
-    <a href="cadlivros.php" class="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50">
-    <span class="absolute left-0 block w-full h-0 transition-all bg-indigo-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-    <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
-        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
-        </svg>
-    </span>
-    <span class="relative">Cadastro de Livros</span>
-    </a>
-</div>
-
-<!--Consulta de Autores-->
-<div class="border w-full h-40 flex items-center justify-center">
+  </div>
+  <div class="margendireita">
+    <!--Consulta de Autores-->
     <a href="consultaautores.php" class="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50">
     <span class="absolute left-0 block w-full h-0 transition-all bg-indigo-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
     <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
@@ -63,10 +49,26 @@
     </span>
     <span class="relative">Consulta de Autores</span>
     </a>
+  </div>
 </div>
 
-<!--Consulta de Livros-->
+<!--Cadastro de Livros-->
 <div class="border w-full h-40 flex items-center justify-center">
+  <div class="margendireita">
+    <!--Cadastro de Livros-->
+    <a href="cadlivros.php" class="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50">
+    <span class="absolute left-0 block w-full h-0 transition-all bg-indigo-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+    <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
+        </svg>
+    </span>
+    <span class="relative">Cadastro de Livros</span>
+    </a>
+  </div>
+
+  <!--Consulta de Livros-->  
+  <div class="margendireita">
     <a href="consultalivros.php" class="relative inline-flex items-center px-12 py-3 overflow-hidden text-lg font-medium text-indigo-600 border-2 border-indigo-600 rounded-full hover:text-white group hover:bg-gray-50">
     <span class="absolute left-0 block w-full h-0 transition-all bg-indigo-600 opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
     <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
@@ -76,7 +78,12 @@
     </span>
     <span class="relative">Consulta de Livros</span>
     </a>
+  </div>
 </div>
+
+
+
+
 
 </body>
 </html>
