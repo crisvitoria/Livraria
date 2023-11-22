9792fecc38963c03a1ee81068765782081335451
crisvitoria <crisa.vitoria@hotmail.com>
2023-11-21 23:52:18 -0300

    adicionado novas linhas de codigo

--- consultalivros.php
+++ consultalivros.php
@@ -2,15 +2,16 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Autores</title>
+    <title>Consulta de Livros</title>
     <link rel="shortcut icon" href="imagens/favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
 <body>
-    <?php
+
+
+<?php
+
     include "conexaobanco.php";
 
     //Query para trazer os generos
@@ -26,22 +27,22 @@
 
     <!--id_livro	id_autor	titulo	fk_genero	data_publicacao	-->
     <div class="w3-container w3-teal">
-        <h2>Cadastro de Livros</h2>
+        <h2>Consulta de Livros</h2>
     </div>
 
-    <form action="cadlivrosscript.php" method="post" class="w3-container">
+    <form action="consultalivrosscript.php" method="post" class="w3-container">
         <br>
         <label class="w3-text-teal"><b>Titulo</b></label>
-        <input class="w3-input w3-border w3-light-grey" type="text" name="titulo" required>
+        <input class="w3-input w3-border w3-light-grey" type="text" name="titulo">
 
         
         <br>
 
         <label class="w3-text-teal"><b>Data de Publicação</b></label>
-        <input class="w3-input w3-border w3-light-grey" type="date" name="data_publicacao" required>
+        <input class="w3-input w3-border w3-light-grey" type="date" name="data_publicacao">
         <br>
         <label class="w3-text-teal"><b>Gênero</b></label>
-        <select class="w3-select" name="genero" required>
+        <select class="w3-select" name="genero">
             <option value="" disabled selected>Selecione</option>
             <?php
             if ($result) {
@@ -58,7 +59,7 @@
         </select>
         <br><br>
         <label class="w3-text-teal"><b>Autor</b></label>
-        <select class="w3-select" name="autor" required>
+        <select class="w3-select" name="autor">
             <option value="" disabled selected>Selecione</option>
             <?php
             if ($result) {
@@ -75,7 +76,7 @@
         </select>
 
         <br><br>
-        <button class="w3-btn w3-blue-grey" name="cadastrar" target="_self" rel="next">Cadastrar</button>
+        <button class="w3-btn w3-blue-grey" name="consultar">Consultar</button>
         <button class="w3-btn w3-blue-grey" type = "reset">Limpar</button>
         <a class="w3-btn w3-blue-grey" href="index.php" target="_self" rel="prev">Página Inicial</a>
 
